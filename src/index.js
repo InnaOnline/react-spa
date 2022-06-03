c091d44aa102508d27d271435bb7f7740b5a5ae7
unknown <https://github.com/InnaOnline/todo.git>
2022-06-03 20:18:06 +0300

    gh-pages added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/react-spa">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
